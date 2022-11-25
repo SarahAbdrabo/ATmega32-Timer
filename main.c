68fad1eaa06dcb25d965219820b1160a0e1f7443
Sarah Abdrabo <sarah.m.abdrabo@gmail.com>
2022-11-25 15:17:48 +0200

    app layer

---
+++ main.c
@@ -0,0 +1,20 @@
+/*The App layer of the timer driver, used to test.*/
+
+#include "Timer_Interface.h"
+
+
+
+int main (void){
+
+
+
+
+
+
+
+
+return 0; 
+}
+
+//  servo motor angles
+//	TIMER_OCR1A_REG = 499;
